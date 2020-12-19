6e4b6020a96cadf68355577d4737c3591a7e4eba
Linden Chiu <hchiu87@gmail.com>
2020-12-19 00:40:05 -0800

    add recommendation

--- src/components/Recommendations.js
+++ src/components/Recommendations.js
@@ -1,11 +1,14 @@
 import React from "react";
 
+import imageSrc from '../assets/aloe_vera.png';
+
 const Recommendations = ({ background, title, subtitle }) => (
   <div>
      <h1>Plant Recommendations</h1>
      <h2>Based on your local climate and mental health needs</h2>
      <div>
-     <img title="my-img" src={imageSrc} alt="my-img" /> 
+     <img title="my-img" src={imageSrc} alt="my-img" width={400} height={200}/> 
+     <h4>Getting an aloe vera plant can purify the air in your home</h4>
      </div>
 
 
